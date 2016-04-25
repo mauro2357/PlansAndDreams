eb9d28e026994d9e7c6d59edeecc36447396abee
Juan Mauricio Giraldo <Juan_Giraldo@satrack.com>
2016-04-25 15:06:43 -0500

    Proyecto Base de Prueba Unitarias

---
+++ PlansAndDreams/PlansAndDreams.Core/domain/Sueno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlansAndDreams.Core.domain
+{
+    class Sueno
+    {
+
+        public void agregar()
+        {
+            Console.Out.WriteLine("Sueño Agregado Exitosamente");
+        }
+    }
+}
