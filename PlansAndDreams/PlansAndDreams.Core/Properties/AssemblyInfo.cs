85e12768e4c57598242daadf74d3e5a59772a296
Juan Mauricio Giraldo <Juan_Giraldo@satrack.com>
2016-04-25 16:07:19 -0500

    MSBuuild

--- PlansAndDreams/PlansAndDreams.Core/Properties/AssemblyInfo.cs
+++ PlansAndDreams/PlansAndDreams.Core/Properties/AssemblyInfo.cs
@@ -36,3 +36,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestProject1")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("PlansAndDreams.Tests")]
