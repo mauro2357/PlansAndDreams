3316ffdd4ee07689f61dcd7dc28f3fadfe7d86f7
Juan Mauricio Giraldo <Juan_Giraldo@satrack.com>
2016-04-26 09:52:03 -0500

    SpecFlow Integrado

---
+++ PlansAndDreams/PlansAndDreams.Tests/Acceptance/VisualizarSuenos/VisualizadorSuenosStep.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace PlansAndDreams.Tests.Acceptance.VisualizarSuenos
+{
+    [Binding]
+    public class VisualizadorSuenosStep
+    {
+        [Given(@"Yo ingreso a la pantalla de sueños")]
+        public void GivenYoIngresoALaPantallaDeSuenos()
+        {
+            Console.WriteLine("GivenYoIngresoALaPantallaDeSuenos");
+        }
+
+        [When(@"Yo selecciono como sueño una casa en rionegro")]
+        public void WhenYoSeleccionoComoSuenoUnaCasaEnRionegro()
+        {
+            Console.WriteLine("WhenYoSeleccionoComoSuenoUnaCasaEnRionegro");
+        }
+
+        [Then(@"Deberia aparecer en pantalla (.*)")]
+        public void ThenDeberiaAparecerEnPantalla(int p0)
+        {
+            Console.WriteLine("ThenDeberiaAparecerEnPantalla");
+        }
+    }
+}
---
+++ PlansAndDreams/PlansAndDreams.Tests/Acceptance/VisualizarSuenos/VisualizarSuenos.feature
@@ -0,0 +1,10 @@
+﻿Feature: VisualizarSuenos
+	Para poder saber en que puedo invertir mis ahorros
+	Como un soñador
+	Quiero conocer cuanto dinero necesito para hacer mi sueño realidad.
+
+@mytag
+Scenario: Sueño con comprar una casa
+	Given Yo ingreso a la pantalla de sueños
+	When Yo selecciono como sueño una casa en rionegro
+	Then Deberia aparecer en pantalla 120000000
