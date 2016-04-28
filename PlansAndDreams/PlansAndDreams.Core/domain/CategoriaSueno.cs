8298839ce4db779861647751b0beb896d9248f64
stiven_londono <stiven_londono@PRISTIVENL>
2016-04-28 16:32:23 -0500

    tdd categoria sueno

---
+++ PlansAndDreams/PlansAndDreams.Core/domain/CategoriaSueno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlansAndDreams.Core.domain
+{
+    public class CategoriaSueno
+    {
+        private string _NombreCategoria;
+
+        public string NombreCategoria
+        {
+            get { return _NombreCategoria; }
+            set { _NombreCategoria = value; }
+        }
+
+
+    }
+}
