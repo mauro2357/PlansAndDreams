ccb224e4e99f2f4d85bb92f309ec99783efb7a67
stiven_londono <stiven_londono@PRISTIVENL>
2016-04-29 14:12:19 -0500

    entidad de ventas

---
+++ PlansAndDreams/PlansAndDreams.Core/domain/Venta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlansAndDreams.Core.domain
+{
+    public class Venta
+    {
+        private Sonador _Sonador;
+
+        public Sonador Sonador
+        {
+            get { return _Sonador; }
+            set { _Sonador = value; }
+        }
+
+        private List<Producto> _Productos;
+
+        public List<Producto> Productos
+        {
+            get { return _Productos; }
+            set { _Productos = value; }
+        }
+
+        private Decimal _ValorComision;
+        public Decimal ValorComision
+        {
+            get { return _ValorComision; }
+            set { _ValorComision = value; }
+        }
+
+        public Decimal ObtenerComision()
+        {
+            return Decimal.MaxValue;
+        }
+    }
+}
---
+++ PlansAndDreams/PlansAndDreams.Tests/Unit/VentaTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlansAndDreams.Core.domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlansAndDreams.Tests.Unit
+{
+    [TestClass()]
+    public class VentaTests
+    {
+        [TestMethod()]
+        public void ObtenerComisionTest()
+        {
+            //Arrange
+            Venta target = new Venta();
+            var expected = Decimal.MaxValue;
+
+            List<Producto> productos = new List<Producto>();
+            Producto producto1 = new Producto();
+            producto1.Categoria = new CategoriaSueno { NombreCategoria = "Viaje", IdCategoria = 1, Porcentaje = 10 };
+            producto1.Nombre = "Nombre Producto";
+            productos.Add(producto1);
+            target.Productos = productos;
+                        
+            //Action
+            Decimal actual = target.ObtenerComision();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
