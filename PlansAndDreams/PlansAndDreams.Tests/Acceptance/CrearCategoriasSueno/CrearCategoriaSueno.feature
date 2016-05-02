8cdd25709322575dda24630a5029cbaa14751ead
stiven_londono <stiven_londono@PRISTIVENL>
2016-05-02 10:56:10 -0500

    ATDD

---
+++ PlansAndDreams/PlansAndDreams.Tests/Acceptance/CrearCategoriasSueno/CrearCategoriaSueno.feature
@@ -0,0 +1,11 @@
+﻿Feature: CrearCategoriaSueno
+	Como Facilitador del sueño
+	Quiero registrar las categorias de los sueños
+	Para Poder cobrar dependiendo de la categoria.
+
+@mytag
+Scenario: Grabar categoria viajes
+	Given Acceso a la pantalla de registro de categoria
+	And ingreso la categoria "Viajes"
+	When Presiono grabar
+	Then Deberia aparecer el mensaje "Categoria grabada con exito."
---
+++ PlansAndDreams/PlansAndDreams.Tests/Acceptance/CrearCategoriasSueno/CrearCategoriaSuenosStep.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace PlansAndDreams.Tests.Acceptance.CrearCategoriasSueno
+{
+    [Binding]
+    public class CrearCategoriaSuenosStep
+    {
+
+        IWebDriver driver = new ChromeDriver(@"C:\exc"); /*andres el mejor*/
+
+        [Given(@"Acceso a la pantalla de registro de categoria")]
+        public void GivenAccesoALaPantallaDeRegistroDeCategoria()
+        {
+            driver.Navigate().GoToUrl("http://localhost:9030/PlansAndDreams/CrearCategoriaSueno.aspx");
+        }
+
+        [Given(@"ingreso la categoria ""(.*)""")]
+        public void GivenIngresoLaCategoria(string categoria)
+        {
+            IWebElement categoriaElement = driver.FindElement(By.Id("categoria"));
+            categoriaElement.SendKeys(categoria);
+        }
+
+        [When(@"Presiono grabar")]
+        public void WhenPresionoGrabar()
+        {
+            IWebElement categoriaElement = driver.FindElement(By.Id("grabar"));
+            categoriaElement.Click();
+        }
+
+        [Then(@"Deberia aparecer el mensaje ""(.*)""")]
+        public void ThenDeberiaAparecerElMensaje(string mensaje)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement mensajeEnPantallaElement = wait.Until<IWebElement>(d => d.FindElement(By.Id("mensajeEnPantalla")));
+            String mensajeEnPantalla = mensajeEnPantallaElement.Text;
+            Assert.AreEqual(mensaje, mensajeEnPantalla);
+        }
+
+    }
+}
