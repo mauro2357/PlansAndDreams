72e76a091092113503d61e0154ddde4af5e10c74
stiven_londono <stiven_londono@PRISTIVENL>
2016-05-02 16:21:34 -0500

    web ui

---
+++ PlansAndDreams/PlansAndDreams.WebUI/Forms/CategoriaSueno.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CategoriaSueno.aspx.cs" Inherits="PlansAndDreams.WebUI.Forms.CategoriaSueno" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <label></label>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label2" runat="server" Font-Bold="True" Font-Size="Large" Text="Crear Categoria"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Nombre categoria"></asp:Label>
+            <br />
+            <input type="text" name="categoria" id="categoria" runat="server" />
+            <br />
+            <asp:Button Text="Grabar" runat="server" ID="grabar" OnClick="grabar_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="mensajeEnPantalla" runat="server" ForeColor="Red" />
+        </div>
+    </form>
+</body></html>
---
+++ PlansAndDreams/PlansAndDreams.WebUI/Forms/CategoriaSueno.aspx.cs
@@ -0,0 +1,43 @@
+﻿using PlansAndDreams.Core.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PlansAndDreams.WebUI.Forms
+{
+    public partial class CategoriaSueno : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        //stiven
+        protected void grabar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //CategoriaApplicationService suenoAppService = new CategoriaApplicationService();
+                bool grabadoExito = true;//await suenoAppService.grabarCategoria(categoria.Value);
+                if (grabadoExito)
+                {
+                    mensajeEnPantalla.Text = "Categoria grabada con exito.";
+                }
+                else
+                {
+                    mensajeEnPantalla.Text = "Hubo un error grabando";
+                }
+            }
+            catch (Exception)
+            {
+                mensajeEnPantalla.Text = "Hubo un error grabando";
+            }
+
+
+        }
+
+    }
+}
