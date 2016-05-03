127504cc0cfeeeb1d36868926b6577e3bfd59752
johaljicar <johaljicar@hotmail.com>
2016-05-03 08:35:13 -0500

    Actualizacion de la Feature. Se le cambia el nombre Visualizar Sueño para ListarSuenos

---
+++ PlansAndDreams/PlansAndDreams.Tests/Acceptance/VisualizarSuenos/ListarSuenos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace PlansAndDreams.Tests.Acceptance.VisualizarSuenos
+{
+    [Binding]
+    public class ListarSuenos
+    {
+        
+        IWebDriver driver = new ChromeDriver(@"C:\exc"); /*andres el mejor*/
+    }
+}
