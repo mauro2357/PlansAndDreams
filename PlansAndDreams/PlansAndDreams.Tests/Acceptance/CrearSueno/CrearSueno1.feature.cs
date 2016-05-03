b4b2e0e9139392d9dd4dbd298a950f322a8e84d5
Andres_Nieto <Andres_Nieto@PRIANDRESNI>
2016-05-03 09:09:06 -0500

    Cambio del feature

--- PlansAndDreams/PlansAndDreams.Tests/Acceptance/CrearSueno/CrearSueno1.feature.cs
+++ PlansAndDreams/PlansAndDreams.Tests/Acceptance/CrearSueno/CrearSueno1.feature.cs
@@ -31,7 +31,7 @@ namespace PlansAndDreams.Tests.Acceptance.CrearSueno
         public static void FeatureSetup(Microsoft.VisualStudio.TestTools.UnitTesting.TestContext testContext)
         {
             testRunner = TechTalk.SpecFlow.TestRunnerManager.GetTestRunner();
-            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-US"), "CrearSueno", "Como soñador del sueño\nQuiero dar a conocer mis sueños\nPara hacerlo realidad.", ProgrammingLanguage.CSharp, ((string[])(null)));
+            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-US"), "CrearSueno", "Como soñador del sueño\r\nQuiero dar a conocer mis sueños\r\nPara hacerlo realidad.", ProgrammingLanguage.CSharp, ((string[])(null)));
             testRunner.OnFeatureStart(featureInfo);
         }
         
