c1ff355b8cf7a139c884f2434bc59ebc36e2a004
johaljicar <johaljicar@hotmail.com>
2016-05-03 09:09:55 -0500

    Finalizacion del primer escenario del grupo de los Chingas

--- PlansAndDreams/PlansAndDreams.Tests/Acceptance/VisualizarSuenos/ListarSuenosAmigosStep.cs
+++ PlansAndDreams/PlansAndDreams.Tests/Acceptance/VisualizarSuenos/ListarSuenosAmigosStep.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,34 +12,27 @@ namespace PlansAndDreams.Tests.Acceptance.VisualizarSuenos
     [Binding]
     public sealed class ListarSuenosAmigosStep
     {
+        IWebDriver driver = new ChromeDriver(@"C:\exc"); /*andres el mejor*/
+        IWebElement categoriaElement;
+
         // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
-
-        [Given("I have entered (.*) into the calculator")]
-        public void GivenIHaveEnteredSomethingIntoTheCalculator(int number)
+        [Given(@"Ingresar a la pantalla de visualizar sueños de amigos")]
+        public void GivenIngresarALaPantallaDeVisualizarSuenosDeAmigos()
         {
-            //TODO: implement arrange (precondition) logic
-            // For storing and retrieving scenario-specific data see http://go.specflow.org/doc-sharingdata 
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
-
-            ScenarioContext.Current.Pending();
+            driver.Navigate().GoToUrl("http://localhost/PlansAndDreams.WebUI/Forms/VisualizarSuenosAmigos.aspx");
         }
 
-        [When("I press add")]
-        public void WhenIPressAdd()
+        [When(@"Cargue la pantalla de visualizar sueños de amigos")]
+        public void WhenCargueLaPantallaDeVisualizarSuenosDeAmigos()
         {
-            //TODO: implement act (action) logic
-
-            ScenarioContext.Current.Pending();
+            categoriaElement = driver.FindElement(By.Id("GrdListaSuenosAmigos"));
         }
 
-        [Then("the result should be (.*) on the screen")]
-        public void ThenTheResultShouldBe(int result)
+        [Then(@"Carga en una tabla la lista de sueños de mis amigos")]
+        public void ThenCargaEnUnaTablaLaListaDeSuenosDeMisAmigos()
         {
-            //TODO: implement assert (verification) logic
-
-            ScenarioContext.Current.Pending();
+            Assert.IsNotNull(categoriaElement);
+            driver.Close();
         }
     }
 }
